d50f60b65b2e8784168c28d209f0df21bd4ff832
Stephen Fraser <stephen.fraser@digirati.co.uk>
2017-04-25 15:52:04 +0100

    Initial code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-export default 'Welcome to bem-js'
+const BEM = {
+  block: (b) => {
+    const modifier = function (m, c) {
+      if (m.toString() === '[object Object]') return this.ms(m);
+      if (!c && c !== undefined) {
+        return this;
+      }
+      return `${this} ${this}--${m}`;
+    };
+    const modifiers = function (m, join = true) {
+      let ms = [this];
+      for (let k in m) {
+        if (m.hasOwnProperty(k) && m[k]) {
+          ms.push(`${this}--${k}`);
+        }
+      }
+      return join ? ms.join(' ') : ms;
+    };
+    // Block.Element
+    b.__proto__.e = b.__proto__.element = function (e) {
+      const be = `${this}__${e}`;
+      // Block.Element.Modifier
+      be.__proto__.m = be.__proto__.modifier = modifier;
+      be.__proto__.ms = be.__proto__.modifiers = modifiers;
+      return be;
+    };
+    // Block.Modifier
+    b.__proto__.m = b.__proto__.modifier = modifier;
+    // Block.Modifier
+    b.__proto__.ms = b.__proto__.modifiers = modifiers;
+    // Block.
+    return b;
+  },
+};
+BEM.b = BEM.block;
+
+export default BEM;
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,9 +1,47 @@
-import expect from 'expect'
+import expect from 'expect';
 
-import message from 'src/index'
+import BEM from 'src/index';
 
-describe('Module template', () => {
-  it('displays a welcome message', () => {
-    expect(message).toContain('Welcome to bem-js')
-  })
-})
+describe('BEM js', () => {
+  it('create block', () => {
+    expect(`${BEM.block('test-block')}`).toEqual('test-block');
+    expect(`${BEM.b('test-block')}`).toEqual('test-block');
+  });
+  it('create element', () => {
+    const testBlock = BEM.block('test-block');
+    expect(`${testBlock.element('test-element')}`).toEqual('test-block__test-element');
+    expect(`${testBlock.e('test-element')}`).toEqual('test-block__test-element');
+  });
+  it('create element with modifiers', () => {
+    const testBlock = BEM.block('test-block');
+    expect(`${testBlock.modifier('test-modifier')}`).toEqual('test-block test-block--test-modifier');
+  });
+  it('create element with modifiers conditionally', () => {
+    const testBlock = BEM.block('test-block');
+    expect(`${testBlock.modifiers({
+      'not-modifier': false,
+      'test-modifier': true,
+      'also-not-modifier': false,
+      'test-modifier2': true,
+    })}`).toEqual('test-block test-block--test-modifier test-block--test-modifier2');
+  });
+  it('create with element modifier', () => {
+    const testBlock = BEM.block('test-block');
+    expect(`${testBlock.element('test-element').modifier('test-modifier')}`)
+      .toEqual('test-block__test-element test-block__test-element--test-modifier');
+
+    expect(`${testBlock.element('test-element').modifiers({'test-modifier': true, 'test-modifier2': true})}`)
+      .toEqual('test-block__test-element test-block__test-element--test-modifier test-block__test-element--test-modifier2');
+  });
+  it('create with element modifier conditionally', () => {
+    const testBlock = BEM.block('test-block');
+    expect(`${testBlock.element('test-element').modifiers({
+      'not-modifier': false,
+      'test-modifier': true,
+      'also-not-modifier': false,
+      'test-modifier2': true,
+    })}`)
+      .toEqual('test-block__test-element test-block__test-element--test-modifier test-block__test-element--test-modifier2');
+  });
+
+});
